d7f64218eb33528e611eb68592bf0f1f86f78b27
santosh kewat <kewatsan@gmail.com>
2019-03-14 12:14:06 +0530

    few changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `The Beaut Machine`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@thebeautmachine`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
