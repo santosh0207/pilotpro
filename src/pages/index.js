7edb369d0e19ac62fa19be35d0de28065d4563b6
santosh kewat <kewatsan@gmail.com>
2019-03-14 12:41:09 +0530

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people this is a blog site</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div> */}
