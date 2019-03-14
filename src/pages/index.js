cf21e4b6a18699550ae72da1bcae5ded2b8469aa
santosh kewat <kewatsan@gmail.com>
2019-03-14 13:00:25 +0530

    ss

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,9 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people this is a blog site</h1>
-    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-    </div> */}
+    </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
