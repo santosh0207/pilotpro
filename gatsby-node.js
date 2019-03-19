90ccca12e1e301d5035439768a5014eac402c561
santosh kewat <kewatsan@gmail.com>
2019-03-19 07:05:06 +0530

    blog post page added

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,48 +2,6 @@
 //  * Implement Gatsby's Node APIs in this file.
 //  **/
 
-// const path = require('path');
-// exports.createPages =({ graphql,actions}) =>{
-//     const {craetePage} = actions
-//     const postTemplate = path.resolve('src/template/blog-post.js')
-
-//     return graphql(`
-//     {
-//         allMarkdownRemark {
-//             edges {
-//               node {
-//                 html
-//                 frontmatter {
-//                   path
-//                   date
-//                   title
-//                   shortscript
-//                   author
-//                 }
-//                 id
-//                 excerpt
-//               }
-//             }
-//         }
-//     }`).then(res=>{
-//         if(res.errors){
-//             return Promise.reject(res.errors)
-//         }
-//        // Create pages for each markdown file.
-//        res.data.allMarkdownRemark.edges.forEach(({ node }) => {
-//         const path1 = node.frontmatter.path
-//         createPage({
-//           path1,
-//           component: postTemplate,
-//           // In your blog post template's graphql query, you can use path
-//           // as a GraphQL variable to query for data from the markdown file.
-//           context: {
-//             path1,
-//           },
-//         })
-//       })
-//     })
-// }
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
